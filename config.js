5ace684a36264a599c765cf2fe4896069675279b
caiqunsong <caiqunsong@vikadata.com>
2022-08-12 10:30:13 +0800

    upload

--- config.js
+++ config.js
@@ -3,13 +3,13 @@
 const configs = {
   AT_AHEAD:true, // 只有机器人被@时回复
   WX_OPENAI_ONOFF:true, // 微信对话平台开启
-  DIFF_REPLY_ONOFF:false, // 开启不同群个性化回复
+  DIFF_REPLY_ONOFF:false,
   VIKA_ONOFF:true, // 维格表开启
-  WX_TOKEN: 'xxxxxxx', // 微信对话平台token
-  VIKA_TOKEN: 'xxxxxxx', // VIKA维格表token
-  VIKA_SPACENAME:'xxxxxxx',  // VIKA维格表空间名称，修改为自己的空间名称
+  WX_TOKEN: 'SksvL7n1tmdpEuTY1GYTjyJI6bBjQd', // 微信对话平台token
+  VIKA_TOKEN: 'usk2oABAUla9eWopJAn7AgY', // VIKA维格表token
+  VIKA_SPACENAME:'vika维格表new',  // VIKA维格表空间名称，修改为自己的空间名称
   VIKA_DATASHEETNAME:'xxxxxxx', // VIKA维格表名称,可以修改为自己的表名称
-  linkWhiteList: ['ledongmao', 'xxxxxxx'],  // 群内链接检测白名单，白名单里成员发布的卡片、链接消息不提示
+  linkWhiteList: ['ledongmao'],  // 群内链接检测白名单，白名单里成员发布的卡片、链接消息不提示
   welcomeList:['25108313781@chatroom', '25187527247@chatroom', '20641535286@chatroom'],
   roomWhiteList: [
     {
@@ -23,7 +23,7 @@ const configs = {
       id: '22064763@chatroom',
     },
   ],  // 群白名单，白名单里内群开启机器人，其他群不开启，暂未实现2022-5-21
-  imOpen: false,  // 是否开启uve-im客户端，设置为true时，需要先 cd ./vue-im 然后 npm install 启动服务 npm run dev
+  imOpen: true,  // 是否开启uve-im客户端，设置为true时，需要先 cd ./vue-im 然后 npm install 启动服务 npm run dev
   imConfigData: {
     socket: '',
     chatInfoEn: {
